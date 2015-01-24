6490d89261dd0cd46a5ed81c9ac29968e042699b
Magus Zeal <meernik@live.com>
2015-01-24 15:22:29 +0000

    not on an airplane anymore

--- util.js
+++ util.js
@@ -55,25 +55,3 @@ var Class = (function(defaults, methods, constructor) {
         return ret;
     }
 });
-/*
-props
-An object whose own enumerable properties constitute descriptors for the properties to be defined or modified. Properties have the following optional keys:
-configurable
-true if and only if the type of this property descriptor may be changed and if the property may be deleted from the corresponding object.
-Defaults to false.
-enumerable
-true if and only if this property shows up during enumeration of the properties on the corresponding object.
-Defaults to false.
-value
-The value associated with the property. Can be any valid JavaScript value (number, object, function, etc).
-Defaults to undefined.
-writable
-true if and only if the value associated with the property may be changed with an assignment operator.
-Defaults to false.
-get
-A function which serves as a getter for the property, or undefined if there is no getter. The function return will be used as the value of property.
-Defaults to undefined.
-set
-A function which serves as a setter for the property, or undefined if there is no setter. The function will receive as only argument the new value being assigned to the property.
-Defaults to undefined.
-*/
