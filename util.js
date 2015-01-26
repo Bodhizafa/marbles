f4425fe1298bb14969906d1cd1fb97713f5d91de
Magus Zeal <meernik@live.com>
2015-01-26 00:52:41 +0000

    make it better, even more tests

--- util.js
+++ util.js
@@ -25,7 +25,7 @@ var close_enough = function(a, b, eps) {
 var sieve = function(A, B) {
     var ret = {}
     Object.keys(B).forEach(function(k) {
-        ret[k] = A[k] === undefined ? B[k] : A[k]
+        ret[k] = ((A === undefined) || (A[k] === undefined)) ? B[k] : A[k]
     });
     return ret;
 }
@@ -38,47 +38,68 @@ var Class = (function(defaults, methods, constructor, properties) {
     }
     Object.freeze(methods);
     return function(desc) {
-        var props = sieve(desc, defaults);
         var ret; // becomes this
         var privates = {};
+        // Make a prototype
         var proto = Object.create(null);
         Object.keys(methods).forEach(function(k) {
-            if (k == "") {
-                return;
-            }
+            if (k == "") return; // functors don't go in props
+
             proto[k] = function() {
-                arguments[arguments.length] = privates;
-                return methods[k].apply(ret, arguments);
-            }
-        });
-        Object.freeze(proto);
-        var propsDesc = {};
-        Object.keys(props).forEach(function(key) {
-            if (properties and properties[key]) {
-                propsDesc[key] = properties[key];
-                if (propsDesc[key].value) throw "Can't specify value in properties. Use defaults.";
-                propsDesc[key].value = props[key];
-            } else {
-                propsDesc[key] = {
-                    "value": props[key],
-                    "writable": true
+                if (arguments.length > 0) {
+                    var args = Array.prototype.slice.call(arguments)
+                    args.unshift(privates)
+                    return methods[k].apply(ret, args);
+                } else {
+                    return methods[k].call(ret, privates);
                 }
             }
         });
+        Object.freeze(proto);
+        // Make the property descriptors
+        var props = sieve(desc, defaults);
+        var propsDesc = {};
+        Object.keys(props).forEach(function(key) {
+            if (properties && properties[key]) {
+                propsDesc[key] = properties[key];
+                if (propsDesc[key].value) throw "Can't specify value in properties. Use defaults.";
+                // If getters and setters are set, make sure that value is null and don't set value in the descriptor
+                if (properties[key].get || properties[key].set) {
+                    if (props[key] !== null) throw "Magic properties must have their defaults entry null";
+                } else {
+                    propsDesc[key].value = props[key];
+                }
+            } else {
+                propsDesc[key] = {
+                    "value": props[key],
+                    "writable": true,
+                    "enumerable": true,
+                }
+            }
+        });
+        // Create the object, set prototype and attach properties
         if (functor) {
             ret = function() {
-                arguments[arguments.length] = privates;
-                return functor.apply(ret, arguments);
+                if (arguments.length > 0) {
+                    var args = Array.prototype.slice.call(arguments)
+                    args.unshift(privates)
+                    return functor.apply(ret, args);
+                } else {
+                    return functor.call(ret, privates);
+                }
             }
             ret.__proto__ = proto;  // XXX probably not a great idea
-            Object.defineProperties(ret, props);
+            Object.defineProperties(ret, propsDesc);
         } else {
             ret = Object.create(proto, propsDesc);
         }
+        // Call the constructor
         if (constructor) {
             constructor.call(ret, privates);
         }
+        // Seal it up ans ship it out.
         Object.seal(ret);
+        Object.seal(privates);
         return ret;
     }
 });
