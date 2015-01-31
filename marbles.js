03e9f8f45bc33cca0a6ef4a211973e930b2395d3
Magus Zeal <meernik@live.com>
2015-01-31 00:01:00 +0000

    Yay no syntax errors!

---
+++ marbles.js
@@ -0,0 +1,45 @@
+"use strict";
+var PrimitiveMarble = Class(
+    // Properties
+    {},
+    // Methods
+    {"": function(priv, arg) {
+        if (arg === undefined) {
+            return priv.value;
+        } else {
+            arg = priv.constraint(arg);
+            var oldVal = priv.value;
+            priv.value = arg;
+            if (priv.compare.call(this, arg, priv.value)) {
+                priv.yell.call(this, priv.value, oldVal);
+            }
+        }
+    }, "addListener": function(priv, listener) {
+        priv.listeners.push(listener);
+    }, "subtractListener": function(priv, listener) {
+        var found_idx = null;
+        priv.listeners.forEach(function(l, idx) {
+            if (l === listener) {
+                found_idx = idx;
+            }
+        })
+        delete priv.listeners[found_idx];
+    }},
+    // Constructor
+    function(priv) {
+        priv.value = undefined;
+        priv.listeners = [];
+        // Functions on priv should always be invoked with the same 'this' as this function, i.e. the object
+        priv.yell = function() { 
+            var args = arguments;
+            priv.listeners.forEach(function(listener, idx) {
+                listener.apply(this, args);
+            }, this);
+        };
+        priv.constraint = function(arg) { return arg; }; // Subclasses override this
+        priv.compare = function(oldVal, newVal) { // And this
+            return oldVal === newVal;
+        };
+    }
+    // Descriptors
+)
